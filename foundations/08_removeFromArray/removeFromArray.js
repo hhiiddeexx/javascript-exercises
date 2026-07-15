4cd65dccbb5179345593d9fc2568114fc581a80e
hhiiddeexx <274948382+hhiiddeexx@users.noreply.github.com>
2026-07-15 08:31:08 -0500

    July 15, 2026: Day 64 - Complete 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  // return arr.filter((item) => a !== item && b !== item);
+  // Solution
+  return arr.filter((item) => !args.includes(item));
 };
+
+let arr = ([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
