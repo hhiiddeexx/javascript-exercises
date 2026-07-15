0b154831228bbccd0148a13b5913b84a64e65a1b
hhiiddeexx <274948382+hhiiddeexx@users.noreply.github.com>
2026-07-15 09:35:26 -0500

    July 15, 2026: Day 64 - Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  const start = Math.min(min, max);
+  const end = Math.max(min, max);
+  const count = end - start + 1;
+  if (min < 0 || max < 0) return "ERROR";
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  return (count * (start + end)) / 2;
 };
+
+sumAll(2, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
